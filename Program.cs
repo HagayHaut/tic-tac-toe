17f50687c1c05181298f45c9deeeacd592a2db85
Hagay Haut <hagayhaut@gmail.com>
2022-12-03 15:24:36 -0500

    move welcome from program to tictactoe class

--- Program.cs
+++ Program.cs
@@ -5,24 +5,9 @@ namespace TicTacToeGame
     {
         static void Main(string[] args)
         {
-            PrintWelcomeAndPositions();
             TicTacToe ttt = new TicTacToe();
+            ttt.PrintWelcomeAndPositions();
             ttt.Play();
-        }
-
-        static void PrintWelcomeAndPositions()
-        {
-            Console.WriteLine("\n\n**************************************************\n");
-            Console.WriteLine(" ____  ____  ___    ____   __    ___    ____  _____  ____ ");
-            Console.WriteLine("(_  _)(_  _)/ __)  (_  _) /__\\  / __)  (_  _)(  _  )( ___)");
-            Console.WriteLine("  )(   _)(_( (__     )(  /(__)\\( (__     )(   )(_)(  )__) ");
-            Console.WriteLine(" (__) (____)\\___)   (__)(__)(__)\\___)   (__) (_____)(____)\n");
-            Console.WriteLine("\n  GRID POSITIONS");
-            Console.WriteLine("\n    1 | 2 | 3 ");
-            Console.WriteLine("   -----------");
-            Console.WriteLine("    4 | 5 | 6 ");
-            Console.WriteLine("   -----------");
-            Console.WriteLine("    7 | 8 | 9 \n");
         }
     }
 }
--- TicTacToe.cs
+++ TicTacToe.cs
@@ -24,6 +24,21 @@ namespace TicTacToeGame
             moveCount = 0;
         }
 
+        public void PrintWelcomeAndPositions()
+        {
+            Console.WriteLine("\n\n**************************************************\n");
+            Console.WriteLine(" ____  ____  ___    ____   __    ___    ____  _____  ____ ");
+            Console.WriteLine("(_  _)(_  _)/ __)  (_  _) /__\\  / __)  (_  _)(  _  )( ___)");
+            Console.WriteLine("  )(   _)(_( (__     )(  /(__)\\( (__     )(   )(_)(  )__) ");
+            Console.WriteLine(" (__) (____)\\___)   (__)(__)(__)\\___)   (__) (_____)(____)\n");
+            Console.WriteLine("\n  GRID POSITIONS");
+            Console.WriteLine("\n    1 | 2 | 3 ");
+            Console.WriteLine("   -----------");
+            Console.WriteLine("    4 | 5 | 6 ");
+            Console.WriteLine("   -----------");
+            Console.WriteLine("    7 | 8 | 9 \n");
+        }
+
         private void DisplayBoard()
         {
             Console.WriteLine($"\n    {@board[0]} | {@board[1]} | {@board[2]} ");
